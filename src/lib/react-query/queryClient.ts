02904d3d5b5bf924342d2002bafb0fa96ef9854f
Ryan-Zayne <phoenixastro97@gmail.com>
2026-04-22 14:08:07 +0100

    .

--- src/lib/react-query/queryClient.ts
+++ src/lib/react-query/queryClient.ts
@@ -1,24 +1,10 @@
-import { defaultShouldDehydrateQuery, QueryClient } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import { isServer } from "@zayne-labs/toolkit-core";
 import { cache } from "react";
 
 const makeQueryClient = () => {
 	return new QueryClient({
 		defaultOptions: {
-			dehydrate: {
-				// include pending queries in dehydration
-				shouldDehydrateQuery: (query) => {
-					return defaultShouldDehydrateQuery(query) || query.state.status === "pending";
-				},
-				shouldRedactErrors: () => {
-					// We should not catch Next.js server errors
-					// as that's how Next.js detects dynamic pages
-					// so we cannot redact them.
-					// Next.js also automatically redacts errors for us
-					// with better digests.
-					return false;
-				},
-			},
 			queries: {
 				retry: 0,
 			},
