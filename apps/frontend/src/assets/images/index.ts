563c1f1c3e64660fc2334604c5c6f36699647940
Ryan-Zayne <phoenixastro97@gmail.com>
2026-04-29 16:49:05 +0100

    done with hero

--- apps/frontend/src/assets/images/index.ts
+++ apps/frontend/src/assets/images/index.ts
@@ -1,3 +1,4 @@
 export { default as authBg } from "./auth-bg.svg?responsive";
 export { default as logoInverted } from "./logo-inverted.svg?responsive";
 export { default as logo } from "./logo.svg?responsive";
+export { default as heroImage } from "./hero-image.png?responsive";
--- apps/frontend/src/pages/(home)/page.tsx
+++ apps/frontend/src/pages/(home)/page.tsx
@@ -1,9 +1,51 @@
+import { heroImage } from "@/assets/images";
+import { IconBox } from "@/components/common/IconBox";
+import { ImageResponsive } from "@/components/common/Image";
+import { NavLink } from "@/components/common/NavLink";
+import { Button } from "@/components/ui";
 import { Main } from "./-components/Main";
 
 function HomePage() {
 	return (
 		<Main>
-			<h1>Home Page</h1>
+			<section className="flex flex-col items-center pt-4 pb-[64px] text-center">
+				<h1
+					className="max-w-[16ch] animate-fade-up font-inter text-[70px]/[72px] font-extrabold
+						tracking-tight text-balance text-black delay-60"
+				>
+					Never run out of{" "}
+					<span className="font-serif font-extrabold text-vitastock-218-100-39 italic">
+						essential drugs
+					</span>{" "}
+					again.
+				</h1>
+
+				<p className="mt-6 animate-fade-up text-[20px] font-medium text-balance delay-120 md:text-xl">
+					Real-time stock tracking, expiry alerts, and fast inventory workflows, built for busy
+					pharmacies and dispensaries.
+				</p>
+
+				<div className="mt-8 flex animate-fade-up flex-col items-center gap-4 delay-180">
+					<Button
+						className="rounded-full
+							shadow-[0_20px_60px_-10px_theme(--color-vitastock-218-100-39/0.35)]"
+						asChild={true}
+					>
+						<NavLink to="/auth/signup">
+							Get Started for Free
+							<IconBox icon="lucide:arrow-right" />
+						</NavLink>
+					</Button>
+					<p className="text-[14px] font-medium">No setup stress. Get started in minutes.</p>
+				</div>
+
+				<div
+					className="mt-10 w-full rounded-[28px] border border-shadcn-border/60 bg-shadcn-muted p-2
+						shadow-lg"
+				>
+					<ImageResponsive src={heroImage} alt="Hero Image" className="rounded-[20px] bg-cover" />
+				</div>
+			</section>
 		</Main>
 	);
 }
