b655aafd3071533de961e4fdffb42d61fdaf816e
Ryan-Zayne <phoenixastro97@gmail.com>
2026-05-14 13:43:20 +0100

    .

--- apps/frontend/src/pages/(home)/layout.tsx
+++ apps/frontend/src/pages/(home)/layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router";
-import { LoadingScreen } from "@/components/common/LoadingScreen";
 import { Footer } from "./-components/Footer";
 import { NavBar } from "./-components/NavBar";
 
 function HomeLayout() {
 	return (
 		<>
-			<LoadingScreen isVisible={true} loadingText="Welcome to Vitastock" />
 			<NavBar />
 			<Outlet />
 			<Footer />
